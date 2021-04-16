1cb1252321b98973139960ca2cfd688ef4b25915
jsososo <jsososo@outlook.com>
2021-04-16 10:40:09 +0800

    🥒 更新获取歌曲链接接口

--- util/SongSaver.ts
+++ util/SongSaver.ts
@@ -3,6 +3,7 @@ import {request} from './request';
 import CrypotJs = require('crypto-js');
 import JsEncrypt = require('node-jsencrypt');
 import SongInfo = Validation.SongInfo;
+import SongUrlMap = Validation.SongUrlMap;
 
 export default class SongSaver {
   constructor() {
@@ -40,51 +41,62 @@ export default class SongSaver {
 
       const obj: Validation.SongInfo = data;
 
-      if (!obj.flac) {
-        // 一套神秘的加密环节！
-        const publicKey = `------BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8asrfSaoOb4je+DSmKdriQJKW
-VJ2oDZrs3wi5W67m3LwTB9QVR+cE3XWU21Nx+YBxS0yun8wDcjgQvYt625ZCcgin
-2ro/eOkNyUOTBIbuj9CvMnhUYiR61lC1f1IGbrSYYimqBVSjpifVufxtx/I3exRe
-ZosTByYp4Xwpb1+WAQIDAQAB
------END PUBLIC KEY-----`;
-        const o = `{"copyrightId":"${cid}","auditionsFlag":0,"type":3}`;
-        const s = new JsEncrypt;
-        s.setPublicKey(publicKey);
-        const a = 1e3 * Math.random();
-        const u = CrypotJs.SHA256(String(a)).toString();
-        const c = CrypotJs.lib.Cipher._createHelper(CrypotJs.algo.AES).encrypt(o, u).toString();
-        const f = s.encrypt(u);
-        const result = await req.send({
-          url: 'https://music.migu.cn/v3/api/music/audioPlayer/getPlayInfo',
-          data: {
-            dataType: 2,
-            data: c,
-            secKey: f,
-          },
-          headers: {
-            referer: 'http://music.migu.cn/v3/music/player/audio',
-            channel: '0146951',
-            uid: 1234,
-          }
-        });
-        obj.flac = `http:${result.data.playUrl.replace(/\?.+$/, '')}`;
-      }
+//       if (!obj.flac) {
+//         // 一套神秘的加密环节！
+//         const publicKey = `------BEGIN PUBLIC KEY-----
+// MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8asrfSaoOb4je+DSmKdriQJKW
+// VJ2oDZrs3wi5W67m3LwTB9QVR+cE3XWU21Nx+YBxS0yun8wDcjgQvYt625ZCcgin
+// 2ro/eOkNyUOTBIbuj9CvMnhUYiR61lC1f1IGbrSYYimqBVSjpifVufxtx/I3exRe
+// ZosTByYp4Xwpb1+WAQIDAQAB
+// -----END PUBLIC KEY-----`;
+//         const o = `{"copyrightId":"${cid}","auditionsFlag":0,"type":3}`;
+//         const s = new JsEncrypt;
+//         s.setPublicKey(publicKey);
+//         const a = 1e3 * Math.random();
+//         const u = CrypotJs.SHA256(String(a)).toString();
+//         const c = CrypotJs.lib.Cipher._createHelper(CrypotJs.algo.AES).encrypt(o, u).toString();
+//         const f = s.encrypt(u);
+//         const result = await req.send({
+//           url: 'https://music.migu.cn/v3/api/music/audioPlayer/getPlayInfo',
+//           data: {
+//             dataType: 2,
+//             data: c,
+//             secKey: f,
+//           },
+//           headers: {
+//             referer: 'http://music.migu.cn/v3/music/player/audio',
+//             channel: '0146951',
+//             uid: 1234,
+//           }
+//         });
+//         console.log(result.data)
+//         obj.flac = `http:${result.data.playUrl.replace(/\?.+$/, '')}`;
+//       }
 
-      if (!obj.picUrl || !obj.lyric) {
+      if (!obj.picUrl || !obj.url || !obj.picUrl) {
         const sInfo = await req.send({
-          url: `https://m.music.migu.cn/migu/remoting/cms_detail_tag?cpid=${cid}`,
+          url: `https://c.musicapp.migu.cn/MIGUM2.0/v1.0/content/resourceinfo.do?copyrightId=${cid}&resourceType=2`,
         })
-        if (sInfo.data) {
-          obj.picUrl = sInfo.data.picS;
-          obj.bigPicUrl = sInfo.data.picL;
+        if (sInfo.resource && sInfo.resource[0]) {
+          const data = sInfo.resource[0];
+          obj.picUrl = (data.albumImgs || [{ img: '' }])[0].img;
+          obj.bigPicUrl = obj.picUrl;
           obj.cid = cid;
-          obj.id = sInfo.data.songId;
-          obj.name = sInfo.data.songName;
-          obj.url = sInfo.data.listenUrl;
-          obj.artists = (sInfo.data.singerId || []).map((id, i) => ({ id, name: (sInfo.data.singerName || [])[i] || ''}))
-          obj.lyric = sInfo.data.lyricLrc;
-          // obj.pic = sInfo.data.
+          obj.id = data.songId;
+          obj.name = data.songName;
+          const typeMap = {
+            PQ: '128',
+            HQ: '320',
+            SQ: 'flac',
+          };
+          (data.newRateFormats || data.rateFormats || []).forEach(({ formatType, androidUrl, url = androidUrl }) => {
+            if (typeMap[formatType]) {
+              obj[typeMap[formatType]] = url.replace(/ftp:\/\/[^/]+/, 'https://freetyst.nf.migu.cn');
+            }
+          })
+          obj.artists = data.artists;
+
+          data.lrcUrl && (obj.lyric = await req.send({ url: data.lrcUrl }, { dataType: 'raw' }))
         }
       }
 
